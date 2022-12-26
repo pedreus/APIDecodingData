e124a435f71cbfbc8c69641daa2a3249d04b7214
Pedro Erazo <pedro_erazo@gap.com>
2022-12-26 12:19:14 -0500

    Initial Commit

---
+++ APIDecodingData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIDecodingData
+//
+//  Created by Pedro Erazo Acosta on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
